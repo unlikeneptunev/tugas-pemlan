07379e288a564a4470e5c2cae3aa00ba1bea9d43
huayra <133486215+unlikeneptune@users.noreply.github.com>
2025-03-31 16:02:30 +0700

    Update BangunDatar.java

--- 31Mar25/BangunDatar.java
+++ 31Mar25/BangunDatar.java
@@ -1,3 +1,5 @@
+// Abstract class untuk menyimpan method-method yang akan dipakai oleh kelas turunan
+
 public abstract class BangunDatar {
     public abstract double hitungLuas();
     public abstract double hitungKeliling();
